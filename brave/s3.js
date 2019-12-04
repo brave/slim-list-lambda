2579af04654c8d6e12d8d5585d3f7d1a7ebfed41
Harold Spencer Jr <hspencer@brave.com>
2019-12-03 20:33:37 -0800

    Removed segment logic from AWS functions

--- brave/s3.js
+++ brave/s3.js
@@ -15,8 +15,6 @@ const globalS3 = new awsSdkLib.S3({
   region: 'us-west-2'
 })
 
-AWSXRay.enableManualMode()
-
 const list = async (bucket, prefix) => {
   debugLib.verbose(`Listing items in S3: s3://${bucket}/${prefix}/*`)
   const s3Query = {
@@ -24,13 +22,8 @@ const list = async (bucket, prefix) => {
     Prefix: prefix
   }
 
-  const seg = new AWSXRay.Segment('S3ListFunction');
-  const s3_seg = seg.addNewSubsegment('S3 List');
-  s3_seg.addAnnotation('list_s3_object', prefix);
-
   const result = await globalS3.listObjectsV2(s3Query).promise()
   debugLib.verbose(`Received ${result.KeyCount} results in S3 for query.`)
-  s3_seg.close() 
 
   const matchingKeys = []
   for (const object of result.Contents) {
@@ -47,13 +40,8 @@ const read = async (bucket, key) => {
     Key: key
   }
 
-  const seg = new AWSXRay.Segment('S3ReadFunction');
-  const s3_seg = seg.addNewSubsegment('S3 Read');
-  s3_seg.addAnnotation('read_s3_object', key);
-
   const result = await globalS3.getObject(s3Query).promise()
   debugLib.verbose(`Received file of type ${result.ContentType} of size ${result.ContentLength}.`)
-  s3_seg.close() 
 
   return result.Body
 }
@@ -66,13 +54,7 @@ const write = async (bucket, key, bufferOrString) => {
     Body: bufferOrString
   }
 
-  const seg = new AWSXRay.Segment('S3WriteFunction');
-  const s3_seg = seg.addNewSubsegment('S3 Read');
-  s3_seg.addAnnotation('write_s3_object', key);
-
   await globalS3.putObject(s3Query).promise()
-  s3_seg.close() 
-
 
   debugLib.verbose(`Wrote file to s3://${bucket}/${key}`)
   return true
--- brave/sqs.js
+++ brave/sqs.js
@@ -15,10 +15,6 @@ const write = async (queue, message) => {
   const sqsClient = new awsSdkLib.SQS({ apiVersion: '2012-11-05' })
   debugLib.verbose(`Writing message ${msgFlat} to SQS: ${queue}`)
 
-  const seg = AWSXRay.getSegment();
-  seg.addAnnotation('sqs_queue', queue);
-  seg.addAnnotation('sqs_message', msgFlat);
-
   const sqsMessage = Object.create(null)
   sqsMessage.QueueUrl = queue
   sqsMessage.MessageBody = msgFlat
