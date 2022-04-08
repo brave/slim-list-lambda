24d590b38372ed2c5ea49aaa35b63015b9333687
Peter Snyder <snyderp@gmail.com>
2022-04-07 21:00:16 -0500

    fetch domains from tranco

---
+++ brave/tranco.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * @file
+ * Helper functions to fetch Tranco determined popular lists.
+ */
+
+const requestPromiseLib = require('request-promise')
+
+/**
+ *
+ */
+const get = async (count = 1000) => {
+  // First figure out what the ID of the current Tranco list is.
+  const trancoId = await requestPromiseLib('https://tranco-list.eu/top-1m-id')
+  const trancoUrl = `https://tranco-list.eu/download/${trancoId}/${count}`
+  const trancoRs = (await requestPromiseLib(trancoUrl)).trim()
+  return [trancoUrl, trancoRs.split('\r\n').map(line => line.split(',')[1])]
+}
+
+module.exports = {
+  get
+}
