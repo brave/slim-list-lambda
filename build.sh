392c20670aee23ca235383715f0d7ea770e2eadc
Harold Spencer Jr. <hspencer@brave.com>
2023-09-25 12:22:22 -0700

    Update build.sh to include nss

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # This script runs in the docker container, to set up the rust toolchain
 # needed to build the adblock-rs node module.
 
-yum install -y openssl-devel;
+yum install -y openssl-devel nss;
 curl https://sh.rustup.rs -sSf > /tmp/sh.rustup.rs;
 sh /tmp/sh.rustup.rs -y;
 source ~/.cargo/env;
 npm config set audit false;
 npm install;
-npm i npm@8 -g;
+#npm i npm@8 -g;
 #npm audit fix;
