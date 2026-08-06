9ff61bcb7604413849a3f3b4884d81f8cd0022cd
mschfh <37435502+mschfh@users.noreply.github.com>
2026-08-06 16:56:25 -0500

    comments

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,6 @@
 
 const { resolveIgnoresFromGitignore } = require('neostandard')
 
-// `standard` implicitly skipped everything in .gitignore; flat config does not,
-// so reproduce that behaviour explicitly (build/, config.js, ...).
 module.exports = require('neostandard')({
   ignores: resolveIgnoresFromGitignore()
 })
